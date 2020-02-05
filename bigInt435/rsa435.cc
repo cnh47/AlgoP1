02b3918dd8ba7910a31995d5c74719f30b5c41be
ChaytonH <cnh47@zips.uakron.edu>
2020-02-05 08:49:53 -0500

    Redid functions for P, Q, N, E, and created function for D

--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -6,9 +6,11 @@
 #include <stdlib.h>
 #include <fstream>
 
-
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
+
+bool isPrime(const BigUnsigned& number);
+BigUnsigned AssignBigInt();
 
 int main(){
 	/* The library throws 'const char *' error messages when things go
@@ -17,37 +19,61 @@ int main(){
 	 * code that uses exceptions. */
 	try {
 
-      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
-      BigUnsigned p = BigUnsigned(1);
-      for (int i=0;i<400;i++) {
-         p = p*10 +rand();
-      }
-      std::cout << "my big1 !!!\n";
-      std::cout << p << std::endl;
-	  std::ofstream pq;
-	  pq.open("p_q.txt");
-	  pq << p << std::endl;
+	//Make random number by time
+	srand(time(NULL));
 
-      BigUnsigned q = BigUnsigned(1);
-      for (int i=0;i<400;i++) {
-         q = q*10 +rand();
-      }
-      std::cout << "my big2 !!!\n";
-      std::cout << q << std::endl;
-	   pq << q << std::endl;
+	//get P prime
+	BigUnsigned P = BigUnsigned(1);
+	P = AssignBigInt();
+	while(!isPrime(P)){
+	  P = AssignBigInt();
+	}
 
-      std::cout << "my big3 = big1*big2 !!!\n";
-      BigUnsigned n = p*q;
-      std::cout << n << std::endl;
-	  std::ofstream en;
-	  en.open("e_n.txt");
-	  std::ofstream dn;
-	  dn.open("d_n.text");
-	  en << n << std::endl;
-	  dn << n << std::endl;
+	//get Q prime
+	BigUnsigned Q = BigUnsigned(1);
+	Q = AssignBigInt();
+	while(!isPrime(P)){
+	  Q = AssignBigInt();
+	}
 
-      std::cout << "my big3/big2 !!!\n";
-      std::cout << n/q << std::endl;
+	//get N
+	BigUnsigned N = (P - 1) * (Q - 1);
+
+	//get e
+	BigUnsigned E = AssignBigInt(), two = 2, remainder = E, q;
+	remainder.divideWithRemainder(two, q);
+	BigUnsigned gcdV = gcd(N, E);
+	// while the gcd is not 1 or the E is not negative
+	while ((gcdV != 1) || (remainder != 1)){
+	    E = AssignBigInt();
+	    remainder = E;
+	    remainder.divideWithRemainder(two, Q);
+	    gcdV = gcd(N, E);
+	}
+
+
+	//get D
+	BigUnsigned D = modinv(E, N);
+
+	//Open Files
+	std::ofstream PQ;
+	std::ofstream EN;
+	std::ofstream DN;
+
+	PQ.open("p_q.txt");
+	EN.open("e_n.txt");
+	DN.open("d_n.txt");
+
+	//Store Variables
+	PQ << P << std::endl;
+	PQ << Q << std::endl;
+
+	EN << E << std::endl;
+	EN << N << std::endl;
+
+	DN << D << std::endl;
+	DN << N << std::endl;
+
 
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
@@ -55,4 +81,27 @@ int main(){
 	}
 
 	return 0;
+}
+
+bool isPrime(const BigUnsigned& number){
+	// the a value in Fermat's test - (a ^ ( p - 1)) % p
+    BigInteger a1 = 2;
+	BigInteger a2 = 3;
+
+    BigUnsigned Test1 = modexp(a1, (number - 1), number);
+    BigUnsigned Test2 = modexp(a2, (number - 1), number);
+
+    if ((Test1 == 1) && (Test2 == 1))
+        return true;
+    else
+        return false;
+}
+
+BigUnsigned AssignBigInt(){
+	BigUnsigned n = BigUnsigned(1);
+	while(n.bitLength() < 100){
+		 n = n * 10 +rand();
+	}
+	return n;
+
 }

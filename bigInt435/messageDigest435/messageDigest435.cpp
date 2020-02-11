9440a93f1c6df101e2a43b811c3500a69a4f59d2
ChaytonH <cnh47@zips.uakron.edu>
2020-02-11 18:37:47 -0500

    Fixed and Working

--- bigInt435/messageDigest435/messageDigest435.cpp
+++ bigInt435/messageDigest435/messageDigest435.cpp
@@ -8,6 +8,8 @@
 #include "sha256.h"
 #include "BigIntegerLibrary.hh"
 
+BigUnsigned getSignatureFromFile(const std::string& file);
+
 
 int main(int argc, char* argv[])
 {
@@ -15,7 +17,6 @@ int main(int argc, char* argv[])
         std::cout << "wrong format! should be \"a.exe s filename\"";
     else {
       std::string filename = argv[2];
-
 
       //read the file
       std::streampos begin,end;
@@ -42,10 +43,6 @@ int main(int argc, char* argv[])
          std::cout << "\n"<<"Need to sign the doc.\n";
          // generate a SHA-256 hash of the content of the file to be signed (e.g., "file.txt", ìfile.exeî)
         std::string shaHash = sha256(memblock);
-        std::ofstream file;
-        file.open("file.txt");
-        file << shaHash;
-        file.close();
 
         // sign / "decrypt" this hash value using the private key stored in d_n.txt;
         std::string dTemp, nTemp;
@@ -55,40 +52,29 @@ int main(int argc, char* argv[])
         BigUnsigned D, N;
         D = stringToBigUnsigned(dTemp);
         N = stringToBigUnsigned(nTemp);
-        std::cout << "MDD: " << D << std::endl;
-        std::cout << "MDN: " << N << std::endl;
 
         BigUnsignedInABase hashMessage = BigUnsignedInABase(shaHash, 16);
         BigUnsigned message = hashMessage;
-        BigUnsigned signature = modexp(message, D, N);
+        BigInteger MessConvert = message;
+        BigUnsigned signature = modexp(MessConvert, D, N);
 
         // Store signature in the file
         std::ofstream signatureLog;
-        signatureLog.open("file.txt.signature");
+        signatureLog.open(filename + ".signature");
         signatureLog << signature;
+        std::cout << "\nDocument Signed" << std::endl;
+
       }
       else {
         std::cout << "\n"<<"Need to verify the doc.\n";
         // generate a SHA-256 hash of the content of the file to be signed (e.g., "file.txt", ìfile.exeî)
         std::string shaHash = sha256(memblock);
-        std::ofstream file;
-        file.open("file.txt");
-        file << shaHash;
-        file.close();
 
         BigUnsignedInABase originMessage = BigUnsignedInABase(shaHash, 16);
-        std::cout << "shahash: " << shaHash << std::endl;
-        std::cout << "originalMessg: " << originMessage << std::endl;
-        BigUnsigned originMes = originMessage; // convert to BigUnsigned
+        BigUnsigned originMes = originMessage;
 
         std::string signatureFile = argv[3];
-        std::ifstream signatureF;
-        signatureF.open("file.txt.signature");
-
-        std::string signatureString;
-        signatureF >> signatureString;
-
-        BigUnsigned signature = stringToBigUnsigned(signatureString);
+        BigUnsigned signature = getSignatureFromFile(signatureFile);
 
         // "encrypt" saved hash value (signature) using the public key
         BigUnsigned E, N;
@@ -100,18 +86,26 @@ int main(int argc, char* argv[])
         PublicKeyFile >> eTemp >> nTemp;
         E = stringToBigUnsigned(eTemp);
         N = stringToBigUnsigned(nTemp);
-
         BigUnsigned encrypt = modexp(signature, E, N);
 
-        std::cout << "Encrypt: " << encrypt << std::endl;
-
         if (originMes == encrypt)
-                std::cout << "Document is authentic" << std::endl;
+                std::cout << "\nDocument is authentic" << std::endl;
             else
-                std::cout << "Document is modified" << std::endl;
+                std::cout << "\nDocument is modified" << std::endl;
 
       }
       delete[] memblock;
     }
     return 0;
+}
+
+BigUnsigned getSignatureFromFile(const std::string& file) {
+    std::ifstream signatureFile;
+    signatureFile.open(file);
+
+    std::string signatureString;
+    signatureFile >> signatureString;
+
+    BigUnsigned signature = stringToBigUnsigned(signatureString);
+    return signature;
 }
--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -9,106 +9,97 @@
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
-bool isPrime(const BigUnsigned& number);
-BigUnsigned AssignBigInt();
+BigInteger findE (BigUnsigned &p, BigUnsigned &q, BigUnsigned &mod);
+BigUnsigned AssignBigInt ();
+BigUnsigned RandomBigInt ();
+bool isPrime (BigUnsigned n);
 
 int main(){
-	/* The library throws 'const char *' error messages when things go
-	 * wrong.  It's a good idea to catch them using a 'try' block like this
-	 * one.  Your C++ compiler might need a command-line option to compile
-	 * code that uses exceptions. */
-	try {
-
-	//Make random number by time
-	srand(time(NULL));
-
-	//get P prime
-	BigUnsigned P = BigUnsigned(1);
-	P = AssignBigInt();
-	while(!isPrime(P)){
-	  P = AssignBigInt();
-	}
-
-	//get Q prime
-	BigUnsigned Q = BigUnsigned(1);
-	Q = AssignBigInt();
-	while(!isPrime(P)){
-	  Q = AssignBigInt();
-	}
-
-	//get N
-	BigUnsigned N = (P - 1) * (Q - 1);
-
-	//get e
-	BigUnsigned E = AssignBigInt(), two = 2, remainder = E, q;
-	remainder.divideWithRemainder(two, q);
-	BigUnsigned gcdV = gcd(N, E);
-	// while the gcd is not 1 or the E is not negative
-	while ((gcdV != 1) || (remainder != 1)){
-	    E = AssignBigInt();
-	    remainder = E;
-	    remainder.divideWithRemainder(two, Q);
-	    gcdV = gcd(N, E);
-	}
+    /* The library throws 'const char *' error messages when things go
+     * wrong.  It's a good idea to catch them using a 'try' block like this
+     * one.  Your C++ compiler might need a command-line option to compile
+     * code that uses exceptions. */
+    try {
+        // Find primes for P and Q
+        BigUnsigned P = AssignBigInt();
+        BigUnsigned Q = AssignBigInt();
 
 
-	//get D
-	BigUnsigned D = modinv(E, N);
+        // Calculate N
+        BigUnsigned N = P * Q;
 
-	//Open Files
-	std::ofstream PQ;
-	std::ofstream EN;
-	std::ofstream DN;
+		// Calculate thetaN
+        BigUnsigned thetaN = ( P - 1 ) * (Q - 1);
 
-	PQ.open("p_q.txt");
-	EN.open("e_n.txt");
-	DN.open("d_n.txt");
+		// Calculate E
+        BigInteger E = findE(P, Q, thetaN);
 
-	//Store Variables
-	PQ << P << std::endl;
-	PQ << Q << std::endl;
+		// Calculate D
+        BigUnsigned D = modinv(E, thetaN);
 
-	EN << E << std::endl;
-	EN << N << std::endl;
+		//Open Files
+		std::ofstream PQ;
+		std::ofstream EN;
+		std::ofstream DN;
+		PQ.open("p_q.txt");
+		EN.open("e_n.txt");
+		DN.open("d_n.txt");
 
-	DN << D << std::endl;
-	DN << N << std::endl;
+		//Store Variables
+		PQ << P << "\n";
+        PQ << Q;
+		EN << E << "\n";
+        EN << N;
+		DN << D << "\n";
+        DN << N;
 
-	//Display Values
-	std::cout << "This is your P: \n" << P << std::endl;
-	std::cout << "This is your Q: \n" << Q << std::endl;
-	std::cout << "This is your E: \n" << E << std::endl;
-	std::cout << "This is your N: \n" << N << std::endl;
-	std::cout << "This is your D: \n" << D << std::endl;
+		//Close Files
+		PQ.close();
+		EN.close();
+		DN.close();
+        std::cout << "Keys Created & Stored\n";
 
-
-	} catch(char const* err) {
-		std::cout << "The library threw an exception:\n"
-			<< err << std::endl;
-	}
-
-	return 0;
+    } catch(char const* err) {
+        std::cout << "The library threw an exception:\n"
+            << err << std::endl;
+    }
+    return 0;
 }
 
-bool isPrime(const BigUnsigned& number){
-	// the a value in Fermat's test - (a ^ ( p - 1)) % p
-    BigInteger a1 = 2;
-	BigInteger a2 = 3;
-
-    BigUnsigned Test1 = modexp(a1, (number - 1), number);
-    BigUnsigned Test2 = modexp(a2, (number - 1), number);
-
-    if ((Test1 == 1) && (Test2 == 1))
-        return true;
-    else
-        return false;
+BigUnsigned AssignBigInt () {
+    while (true) {
+        BigUnsigned bigInt = RandomBigInt();
+        if (isPrime(bigInt))
+            return bigInt;
+    }
 }
 
-BigUnsigned AssignBigInt(){
-	BigUnsigned n = BigUnsigned(1);
-	while(n.bitLength() < 100){
-		 n = n * 10 +rand();
-	}
-	return n;
+BigInteger findE (BigUnsigned &p, BigUnsigned &q, BigUnsigned &mod) {
+    BigUnsigned e;
+    while (true) {
+        e = rand()%90 + 10;
+        if (gcd (e, mod) == 1) {
+            return e;
+        }
+    }
+}
 
+BigUnsigned RandomBigInt () {
+    BigUnsigned bigIntHolder = BigUnsigned(1);
+    for (int i = 0; i < 155; i++) {
+        bigIntHolder = bigIntHolder * 10 + rand();
+    }
+    return bigIntHolder;
+}
+bool isPrime (BigUnsigned n) {
+    BigUnsigned a = BigUnsigned(3);
+    for (a; a <= 9; a++) {
+        // Verify that a and n are co-prime
+        if (gcd(n, a) != 1) {
+            return false;
+        }
+        if (modexp(a, n-1, n) != 1)
+            return false;
+    }
+    return true;
 }
